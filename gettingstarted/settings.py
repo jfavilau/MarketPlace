bf87282a531878ac1a350244c9824eb5448b7f5c
Erick Coral <ej.coral@uniandes.edu.co>
2017-10-02 15:26:50 -0500

    static modifications

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
