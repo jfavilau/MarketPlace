7309b15cb3f0677f7a9fcdb57694c8b4ae703615
Jhon Avila <jf.avila@uniandes.edu.co>
2017-10-13 01:36:54 -0500

    Registro básico de un Productor

--- hello/serializers.py
+++ hello/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Producer
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,8 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'image', 'description', 'unit', 'price')
+
+class ProducerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producer
+        fields = ('id','identificationNumber', 'name', 'address', 'latitude', 'longitude', 'phoneNumber', 'cooperative', 'active')
