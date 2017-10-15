46cba9f12137364877fe0133c5716a0f0f907dfb
cami7ord <cami7ord@gmail.com>
2017-10-14 19:12:04 -0500

    Load products from category from server.
    Load products from category from server.

--- hello/static/js/catalogue.js
+++ hello/static/js/catalogue.js
@@ -4,6 +4,8 @@
     // USE STRICT
     "use strict";
 
+    var categories;
+
     //var baseURL = "https://cors.io/?https://marketplace201720.herokuapp.com/";
     var baseURL = "http://localhost:8000/";
 
@@ -14,12 +16,9 @@
     function downloadCategories() {
         $.getJSON(baseURL + "api/categories/").done(function(data) {
 
-            // Hide loader
-            $('.page-loader').fadeOut('slow', function () {
-                $(this).remove();
-            });
+            categories = data;
 
-            $.each(data, function(i, item) {
+            $.each(categories, function(i, item) {
 
                 var myvar = '<li>' +
                                 '<a href=\"#\" id=\"category' + i +'\">' + item.name + '</a>' +
@@ -32,53 +31,61 @@
 
             });
 
+            selectCategory(0);
+
         });
     }
 
+    function hideLoader() {
+        // Hide loader
+        $('.page-loader').fadeOut('slow', function () {
+            $(this).remove();
+        });}
+
     function selectCategory(pos) {
-        console.log("Position: " + pos);
+
+        var category = categories[pos];
+
+        $("#category_name").text(category.name);
+
+        var productsHTML = "";
+
+        $.each(category.products, function(i, item) {
+
+            productsHTML += '<div class="col-md-4 col-sm-6">'+
+'                            <div class="product-1" style="text-align: center;">'+
+'                                <div class="product-image">'+
+'                                    <div class="image-holder">'+
+'                                        <img src="'+ item.image +'" alt="' + item.name +'" style="width:180px;height:180px;" />'+
+'                                    </div>'+
+'                                    <div class="product-action">'+
+'                                        <div class="product-action-list">'+
+'                                            <div class="action-item">'+
+'                                                <a class="fa fa-search-plus" href="#" data-toggle="modal" data-target="#myModal" data-toggle-tooltip="tooltip" data-placement="top" title="Quick view"></a>'+
+'                                            </div>'+
+'                                            <div class="action-item">'+
+'                                                <a class="fa fa-shopping-cart" href="shopping-cart.html" data-toggle-tooltip="tooltip" data-placement="top" title="Add to cart"></a>'+
+'                                            </div>'+
+'                                            <div class="action-item">'+
+'                                                <a class="fa fa-heart" href="wishlist-page.html" data-toggle-tooltip="tooltip" data-placement="top" title="Add to wishlist!"></a>'+
+'                                            </div>'+
+'                                        </div>'+
+'                                    </div>'+
+'                                </div>'+
+'                                <div class="product-content">'+
+'                                    <h3 class="title">'+
+'                                        <a class="name" href="product-details-1.html">' + item.name +'</a>'+
+'                                    </h3>'+
+'                                    <p class="price">$ ' + item.price + ' / ' + item.unit +'</p>'+
+'                                </div>'+
+'                            </div>'+
+'                        </div>';
+
+        });
+
+        $( "#products_by_category" ).replaceWith( productsHTML );
+
+        hideLoader();
     }
 
 })(jQuery);
-
-
-/*
-
-hamburgerAnimation.on("click", function () {
-                hamburgerAnimation.toggleClass("is-active");
-            });
-
-<div class="col-md-4 col-sm-6">
-                        <!-- .product-1-->
-                        <div class="product-1">
-                            <div class="product-image">
-                                <div class="image-holder">
-                                    <img src="images/product-item-01.jpg" alt="Egg free mayo" />
-                                </div>
-                                <div class="product-status hot">
-                                    <span>HOT</span>
-                                </div>
-                                <div class="product-action">
-                                    <div class="product-action-list">
-                                        <div class="action-item">
-                                            <a class="fa fa-search-plus" href="#" data-toggle="modal" data-target="#myModal" data-toggle-tooltip="tooltip" data-placement="top" title="Quick view"></a>
-                                        </div>
-                                        <div class="action-item">
-                                            <a class="fa fa-shopping-cart" href="shopping-cart.html" data-toggle-tooltip="tooltip" data-placement="top" title="Add to cart"></a>
-                                        </div>
-                                        <div class="action-item">
-                                            <a class="fa fa-heart" href="wishlist-page.html" data-toggle-tooltip="tooltip" data-placement="top" title="Add to wishlist!"></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="product-content">
-                                <h3 class="title">
-                                    <a class="name" href="product-details-1.html">Egg free mayo</a>
-                                </h3>
-                                <p class="price">$ 10.00</p>
-                            </div>
-                        </div>
-                        <!-- end .product-1-->
-                    </div>*/
-
